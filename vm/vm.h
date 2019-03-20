d5abdec5959282ba372acb96432007dbd43a69ed
mmyslyvy <mmyslyvy@student.unit.ua>
2019-03-20 15:53:54 +0200

    Write args splited into two functions

--- vm/vm.h
+++ vm/vm.h
@@ -67,5 +67,7 @@ void				do_turn(t_data *data);
 void				introduce_champs(t_list *champs);
 void				codage_proc(t_process *process, unsigned char codage);
 int 				get_absolute_cord(int cord, int relative_cord);
+void				write_args_pointers(t_data *data, t_process *process);
+int					get_offset(t_process *process);
 
 #endif
