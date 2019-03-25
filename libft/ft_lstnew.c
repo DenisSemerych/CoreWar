76ee0e413c9cea733f9619ba6e0ea65cc6a2e0f6
o4eredko <evgeny.ocheredko@gmail.com>
2019-03-25 23:34:57 +0200

    updated lstnew

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -19,7 +19,20 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	if (!(new = (t_list*)malloc(sizeof(t_list))))
 		return (NULL);
 	new->next = NULL;
-	new->content = (void*)content;
-	new->content_size = content_size;
+	if (content)
+	{
+		if (!(new->content = malloc(content_size)))
+		{
+			free(new);
+			return (NULL);
+		}
+		new->content = ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
+	}
+	else
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
 	return (new);
 }
